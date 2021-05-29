4ad47e44064cfc6780847c56168c2067b924ec43
yeni <halukbilgin.uk@gmail.com>
2021-05-29 18:47:38 +0300

    Initial commit

---
+++ src/test/java/pages/MainPage.java
@@ -0,0 +1,22 @@
+package pages;
+
+import testbase.BaseClass;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import java.util.List;
+
+public class MainPage {
+
+	@FindBy(xpath = "//div[@class='app_logo']")
+	public WebElement appLogo;
+
+	@FindBy(xpath = "//div[@class = 'inventory_item_name']")
+	public List<WebElement> itemList;
+
+	public MainPage() {
+		PageFactory.initElements(BaseClass.driver, this);
+	}
+
+}
